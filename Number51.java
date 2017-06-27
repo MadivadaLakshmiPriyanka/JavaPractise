cc08163bcb09f56f778c9d66b0432c802a5a59c2
MadivadaLakshmiPriyanka <pinky.mlp@gmail.com>
2017-06-27 14:50:42 +0530

    Add files via upload

---
+++ Number51.java
@@ -0,0 +1,21 @@
+
+public class Number51 {
+	public static void main(String args[])
+	{
+		int k=10;
+		for(int i=1;i<=10;i++)
+		{
+			for(int j=1;j<=k;j++)
+			{
+				System.out.print("*");
+			
+			}
+			k--;
+			
+			System.out.println(" ");
+		}
+		
+}
+
+
+}
